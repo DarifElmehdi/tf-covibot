0182c65afc30ce47d06a4245560ee9d255893c76
DarifElmehdi <46763117+DarifElmehdi@users.noreply.github.com>
2021-06-15 18:17:37 +0100

    chatbot init
    First commit

---
+++ server.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, jsonify , request
+from flask_cors import CORS
+from chatbot import get_response
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+@app.route('/chat/<string:user_input>',methods=['GET'])                                                                                                    
+def chat(user_input):                                                                                                                            
+    response = get_response(user_input)
+    return {'msg' : response}
+
+app.run()
